7cfc354bf69a2a469464a2ab9a29a457df6a8334
soundsnick <soundsnick@gmail.com>
2019-02-11 05:18:00 +0600

    asas

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Мы переехали</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <style>
+        @font-face{
+            src: url('Halfomania-Regular.ttf');
+            font-family: Halfomania;
+        }
+        body{
+            margin: 0;
+            padding: 0;
+            font-family: Gotham Pro;
+            background-image: url('flat-space-background.jpg');
+            background-size: cover;
+            text-align: center;
+        }
+        .title{
+            font-family: Halfomania;
+            margin-top: 33vh;
+            font-size: 100px;
+            color: #fff;
+            text-transform: uppercase;
+            text-shadow: 0 1px 45px #f2ff94;        
+            margin-bottom: 0;
+        }
+        a{
+            font-family: Roboto;
+            font-weight: 700;
+            text-decoration: none;
+            color: #fff;
+            background: #F44336;
+            display: inline-block;
+            padding: 20px 110px;
+            text-transform: uppercase;
+            border-radius: 40px;
+            margin-top: 32px;
+            font-size: 13px;
+            box-shadow: 0 27px 29px -17px #ff47477d;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="title">We now on</h1>
+    <a href="https://loren.kz">NEW LINK</a>
+</body>
+</html>
